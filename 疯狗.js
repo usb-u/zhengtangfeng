d55ca7bbfaf206473ed92a2ec2cb20d9d9ef6c82
Your Name <you@example.com>
2020-01-18 04:09:39 +0800

    第一次提交

---
+++ 疯狗.js
@@ -0,0 +1,3 @@
+img.onclick = function() {
+    window.open('file:///C:/Users/Administrator/Desktop/%E6%95%B4%E4%BA%BA/%E8%AF%A6%E6%83%85.html', '_self')
+}
---
+++ 详情.js
@@ -0,0 +1,11 @@
+one.onchange = function() {
+    console.log('用户名:' + one.value)
+}
+
+twe.onchange = function() {
+    console.log('密码'+twe.value)
+}
+
+three.onchange = function() {
+    console.log('手机号'+three.value)
+}
